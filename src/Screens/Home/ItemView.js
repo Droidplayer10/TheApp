77806a0dd39bafe6fb6c6958034fa8df0e31327c
Droidplayer10 <droidplayer10@gmail.com>
2023-04-28 10:48:48 +0200

    Modal
    Create Modal for attendance sheet

--- src/Screens/Home/ItemView.js
+++ src/Screens/Home/ItemView.js
@@ -6,6 +6,8 @@ import {
   Modal,StyleSheet
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 
 const ItemView = ({ item, modalVisible, closeModal, openModal }) => {
@@ -18,17 +20,18 @@ const ItemView = ({ item, modalVisible, closeModal, openModal }) => {
       <View style={styles.ItemView} >
         <Text style={styles.title}>{item.job_name}</Text>
         
-        <TouchableOpacity onPress={openModal}>
+        
           <Text style={styles.joboftext}>Munkaidő kezdete:</Text>
          <Text style={styles.joboftext}>{'\t\t\t\t\t'+formattedStartTime}</Text> 
           <Text style={styles.joboftext}>Munkaidő vége: </Text>
           <Text style={styles.joboftext}>{'\t\t\t\t\t'+formattedEndTime}</Text>
           <Text style={styles.joboftext}>Helyszín: </Text>
-        </TouchableOpacity>
+        
         
         <Text style={styles.joboftext}>{'\t\t\t\t\t'+item.job_location}</Text>
         <Text></Text>
-        <TouchableOpacity style={{alignSelf:'flex-end',}}>
+        <TouchableOpacity style={{alignSelf:'flex-end'}} onPress={openModal}>
+          
         <Text style={{fontSize:20, fontFamily:'sans-serif-medium'}}>Jelenléti ív</Text>
       </TouchableOpacity>
       </View>
@@ -45,9 +48,23 @@ const ItemView = ({ item, modalVisible, closeModal, openModal }) => {
         visible={modalVisible}
         onRequestClose={closeModal}
       >
-        <TouchableOpacity style={styles.modalBox} onPress={closeModal}>
-          <Text style={styles.text}>{item.attendance_student_id}</Text>
-        </TouchableOpacity>
+          {/* -----MODAL ABLAK------- */}
+
+          <View style={styles.modalView}>
+              <Text style={styles.modalTitle}>Jelenléti ív</Text>
+
+          </View>
+
+
+          <TouchableOpacity
+                style={styles.CloseModalButton}
+                onPress={closeModal}
+              >
+                
+              
+                <Text style={styles.CloseModalText}>X</Text>
+
+              </TouchableOpacity>
       </Modal>
     </View>
   );
@@ -65,8 +82,8 @@ const styles = StyleSheet.create({
     width: '95%',
     
     borderRadius: 30,
-    paddingVertical: '20%',
-    paddingHorizontal: 10,
+    paddingVertical: '10%',
+    paddingHorizontal: '5%',
     
   },
  counter: {
@@ -76,7 +93,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     alignItems: 'flex-start',
     width: '80%',
-    height: '15%',
+    height: '12%',
     position: 'absolute',
   bottom: 0,
     overflow: 'hidden',
@@ -95,6 +112,12 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 5,
     borderBottomRightRadius: 5,
   },
+  modalTitle:{
+    fontSize: 25,
+    color:'black',
+    fontWeight:'bold',
+    fontFamily:''
+  },
   title: {
     fontSize: 25,
     fontWeight: 'bold',
@@ -111,16 +134,35 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
   },
-  modalBox: {
-    position: 'absolute',
-    top: '50%',
-    left: '90%',
-    backgroundColor: '#fff',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 5,
-    transform: [{ translateY: -20 }, { translateX: -80 }],
+  modalView: {
+    height: 700,
+    width: 380,
+    margin: 15,
+    backgroundColor: "white",
+    borderRadius: 50,
+    padding: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4.0,
+    elevation: 5,
   },
+  CloseModalButton:{
+    borderRadius: 80,
+    padding: 6,
+    elevation: 2,
+    backgroundColor:'red'
+  },
+  CloseModalText:{
+    fontSize:25,
+    fontWeight:'bold',
+    color: '#2F73FE',
+    textAlign:'center'
+  }
 });
 
 export default ItemView;
